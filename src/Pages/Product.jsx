c85407c4c5447168f149b01cf8b9c778b4c69837
Koki Adachi <kadachi.mys@gmail.com>
2025-08-04 18:33:40 +0800

    Product display page added

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ShopContext } from "../Context/ShopContext";
+import { useParams } from "react-router-dom";
+import { Breadcrum } from "../Components/Breadcrum/Breadcrum";
+import { ProductDisplay } from "../Components/ProductDisplay/ProductDisplay";
 
 export const Product = () => {
-  return <div>Product</div>;
+  const { allProducts } = useContext(ShopContext);
+  const { productId } = useParams();
+  const product = allProducts.find((e) => e.id === Number(productId));
+
+  return (
+    <div>
+      <Breadcrum product={product} />
+      <ProductDisplay product={product} />
+    </div>
+  );
 };
 
 export default Product;
