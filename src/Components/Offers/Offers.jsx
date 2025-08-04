8a30e916d0ddb9041ca8bdfc1886a5f4a4ea1c5d
Koki Adachi <kadachi.mys@gmail.com>
2025-08-04 16:02:39 +0800

    Added offers section

---
+++ src/Components/Offers/Offers.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Offers.css";
+import exclusiveImg from "../Assets/exclusive_image.png";
+
+export default function Offers() {
+  return (
+    <div className="offers">
+      <div className="offers-left">
+        <h1>Exclusive</h1>
+        <h1>Offers for You</h1>
+        <p>ONLY ON BEST SELLERS PRODUCTS</p>
+        <button>Check Now</button>
+      </div>
+      <div className="offers-right">
+        <img src={exclusiveImg} alt="female model" />
+      </div>
+    </div>
+  );
+}
